f0acadb6ef6671a6ac662ba04ab4004514e6cadb
Kevin Michalewicz <44092360+kevinmicha@users.noreply.github.com>
2021-05-07 14:48:06 +0200

    created file in incorrect branch

--- jean_zay/env_configs/exact_recon_unet.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-module purge
-module load tensorflow-gpu/py3/2.2.0
-
-export BSD500_DATA_DIR=$SCRATCH/
-export BSD68_DATA_DIR=$SCRATCH/
-export DIV2K_DATA_DIR=$SCRATCH/
-export LOGS_DIR=$SCRATCH/
-export CHECKPOINTS_DIR=$SCRATCH/
